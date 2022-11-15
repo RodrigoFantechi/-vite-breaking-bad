9fdf91a2789531e26136a2b652ca734f05d9cfb3
RodrigoFantechi <rodrifante595@libero.it>
2022-11-15 09:09:56 +0100

    fix loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -5,8 +5,12 @@ export default {
 </script>
 
 <template>
+  <div class="loader d-flex justify-content-center align-items-center bg-white">
   <img src="../assets/img/Spinner-5.gif" alt="">
+</div>
 </template>
 <style lang="scss" scoped>
-    
+    .loader{
+      height: 800px;
+    }
 </style>
